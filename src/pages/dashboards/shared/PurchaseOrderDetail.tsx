e1e8356d286be74956f44d6b4779a993388554bd
Govind Jayakumar <govindjayakumar858@gmail.com>
2026-05-29 15:12:16 +0530

    bugs in progress

--- src/pages/dashboards/shared/PurchaseOrderDetail.tsx
+++ src/pages/dashboards/shared/PurchaseOrderDetail.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+    import { useState, useEffect, useCallback } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import {
     getPurchaseOrderById,
